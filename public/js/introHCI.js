7748caff3f49c78c167fb018a9f5bf49623c8775
Jiashen Wang <jiashenwang@Jiashens-MacBook-Pro.local>
2017-02-15 22:30:44 -0800

    jw - finished lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,8 +25,19 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	var details = $('#'+projectID+' .details');
 
-	console.log("User clicked on project " + idNumber);
+	$.get("/project/"+idNumber, function(result) {
+		var projectHTML = '<div><img src="' + result['image'] 
+						+ '" class="detailsImage"><p>' + result['title']
+						+ '</p><p><small>' + result['date'] 
+						+ '</small></p><p>' + result['summary'] + '</p></div>'
+
+		details.html(projectHTML);
+		console.log(result);
+	});
+
+	// console.log("User clicked on project " + idNumber);
 }
 
 /*
@@ -34,5 +45,18 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+
+	$.get("/palette", function(result) {
+		var colors = result['colors']['hex'];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
+
+
+	// console.log("User clicked on color button");
 }
+
+
